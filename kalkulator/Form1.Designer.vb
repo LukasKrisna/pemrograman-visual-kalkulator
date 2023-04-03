819356da173302ef5ec101689a98ff4cc0d9a6ca
LukasKrisna <krisnaprasriyan@gmail.com>
2023-04-03 10:11:38 +0700

    first commit

---
+++ kalkulator/Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button0 = New Button()
+        ButtonKoma = New Button()
+        ButtonHasil = New Button()
+        ButtonTambah = New Button()
+        ButtonKurang = New Button()
+        ButtonKali = New Button()
+        ButtonBagi = New Button()
+        ButtonHapus = New Button()
+        LabelHasil = New Label()
+        ButtonPlus_Min = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        Button1.Location = New Point(10, 246)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(88, 58)
+        Button1.TabIndex = 0
+        Button1.Text = "1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        Button2.Location = New Point(104, 246)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(88, 58)
+        Button2.TabIndex = 1
+        Button2.Text = "2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        Button3.Location = New Point(198, 246)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(88, 58)
+        Button3.TabIndex = 2
+        Button3.Text = "3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        Button4.Location = New Point(10, 182)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(88, 58)
+        Button4.TabIndex = 3
+        Button4.Text = "4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        Button5.Location = New Point(104, 182)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(88, 58)
+        Button5.TabIndex = 4
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        Button6.Location = New Point(198, 182)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(88, 58)
+        Button6.TabIndex = 5
+        Button6.Text = "6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        Button7.Location = New Point(10, 118)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(88, 58)
+        Button7.TabIndex = 6
+        Button7.Text = "7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        Button8.Location = New Point(104, 118)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(88, 58)
+        Button8.TabIndex = 7
+        Button8.Text = "8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        Button9.Location = New Point(198, 118)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(88, 58)
+        Button9.TabIndex = 8
+        Button9.Text = "9"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button0
+        ' 
+        Button0.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        Button0.Location = New Point(104, 310)
+        Button0.Name = "Button0"
+        Button0.Size = New Size(88, 58)
+        Button0.TabIndex = 9
+        Button0.Text = "0"
+        Button0.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonKoma
+        ' 
+        ButtonKoma.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonKoma.Location = New Point(198, 310)
+        ButtonKoma.Name = "ButtonKoma"
+        ButtonKoma.Size = New Size(88, 58)
+        ButtonKoma.TabIndex = 10
+        ButtonKoma.Text = ","
+        ButtonKoma.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonHasil
+        ' 
+        ButtonHasil.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonHasil.Location = New Point(292, 310)
+        ButtonHasil.Name = "ButtonHasil"
+        ButtonHasil.Size = New Size(108, 58)
+        ButtonHasil.TabIndex = 11
+        ButtonHasil.Text = "="
+        ButtonHasil.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonTambah
+        ' 
+        ButtonTambah.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonTambah.Location = New Point(292, 246)
+        ButtonTambah.Name = "ButtonTambah"
+        ButtonTambah.Size = New Size(108, 58)
+        ButtonTambah.TabIndex = 12
+        ButtonTambah.Text = "+"
+        ButtonTambah.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonKurang
+        ' 
+        ButtonKurang.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonKurang.Location = New Point(292, 182)
+        ButtonKurang.Name = "ButtonKurang"
+        ButtonKurang.Size = New Size(108, 58)
+        ButtonKurang.TabIndex = 13
+        ButtonKurang.Text = "-"
+        ButtonKurang.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonKali
+        ' 
+        ButtonKali.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonKali.Location = New Point(292, 118)
+        ButtonKali.Name = "ButtonKali"
+        ButtonKali.Size = New Size(108, 58)
+        ButtonKali.TabIndex = 14
+        ButtonKali.Text = "x"
+        ButtonKali.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonBagi
+        ' 
+        ButtonBagi.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonBagi.Location = New Point(292, 54)
+        ButtonBagi.Name = "ButtonBagi"
+        ButtonBagi.Size = New Size(108, 58)
+        ButtonBagi.TabIndex = 15
+        ButtonBagi.Text = "÷"
+        ButtonBagi.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonHapus
+        ' 
+        ButtonHapus.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonHapus.Location = New Point(10, 54)
+        ButtonHapus.Name = "ButtonHapus"
+        ButtonHapus.Size = New Size(276, 58)
+        ButtonHapus.TabIndex = 16
+        ButtonHapus.Text = "C"
+        ButtonHapus.UseVisualStyleBackColor = True
+        ' 
+        ' LabelHasil
+        ' 
+        LabelHasil.AutoSize = True
+        LabelHasil.Dock = DockStyle.Right
+        LabelHasil.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        LabelHasil.Location = New Point(374, 0)
+        LabelHasil.Name = "LabelHasil"
+        LabelHasil.Size = New Size(32, 37)
+        LabelHasil.TabIndex = 17
+        LabelHasil.Text = "0"' 
+        ' ButtonPlus_Min
+        ' 
+        ButtonPlus_Min.Font = New Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point)
+        ButtonPlus_Min.Location = New Point(10, 310)
+        ButtonPlus_Min.Name = "ButtonPlus_Min"
+        ButtonPlus_Min.Size = New Size(88, 58)
+        ButtonPlus_Min.TabIndex = 18
+        ButtonPlus_Min.Text = "+/-"
+        ButtonPlus_Min.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(406, 376)
+        Controls.Add(ButtonPlus_Min)
+        Controls.Add(LabelHasil)
+        Controls.Add(ButtonHapus)
+        Controls.Add(ButtonBagi)
+        Controls.Add(ButtonKali)
+        Controls.Add(ButtonKurang)
+        Controls.Add(ButtonTambah)
+        Controls.Add(ButtonHasil)
+        Controls.Add(ButtonKoma)
+        Controls.Add(Button0)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Kalkulator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents ButtonKoma As Button
+    Friend WithEvents ButtonHasil As Button
+    Friend WithEvents ButtonTambah As Button
+    Friend WithEvents ButtonKurang As Button
+    Friend WithEvents ButtonKali As Button
+    Friend WithEvents ButtonBagi As Button
+    Friend WithEvents ButtonHapus As Button
+    Friend WithEvents LabelHasil As Label
+    Friend WithEvents ButtonPlus_Min As Button
+End Class
